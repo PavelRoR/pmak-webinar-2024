fd235c9ab4116254b0b9b9d9fdb8d0895d32e06d
PavelRoR <pavelvp2182@yandex.ru>
2024-04-05 15:24:59 +0500

    Main page adapting tablets

--- _app/_js/scripts.js
+++ _app/_js/scripts.js
@@ -1,42 +1,42 @@
 $(document).ready(function() {
-    /* Якорь */
-    $("a[href^='#']").click(function (h) {
-        h.preventDefault();
-        var f = $(this).attr("href"),
-            g = $(f).offset().top;
-        $("body,html").animate({
-            scrollTop: g
-        }, 1500)
-    });
-    // Мобильное меню
-    var mobMenu = $('.mobile-menu'),
-    headNav = $('.header-menu'),
-    mml1 = $('.mobile-menu-line-1'),
-    mml2 = $('.mobile-menu-line-2'),
-    mml3 = $('.mobile-menu-line-3');
+//     /* Якорь */
+//     $("a[href^='#']").click(function (h) {
+//         h.preventDefault();
+//         var f = $(this).attr("href"),
+//             g = $(f).offset().top;
+//         $("body,html").animate({
+//             scrollTop: g
+//         }, 1500)
+//     });
+//     // Мобильное меню
+//     var mobMenu = $('.mobile-menu'),
+//     headNav = $('.header-menu'),
+//     mml1 = $('.mobile-menu-line-1'),
+//     mml2 = $('.mobile-menu-line-2'),
+//     mml3 = $('.mobile-menu-line-3');
 
-mobMenu.on('click', function () {
-    if (!headNav.hasClass('nav-active')) {
-        mobMenu.addClass('fixed');
-        headNav.addClass('nav-active');
-        mml1.addClass('switched');
-        mml2.addClass('switched');
-        mml3.addClass('switched');
-    } else {
-        mobMenu.removeClass('fixed');
-        headNav.removeClass('nav-active');
-        mml1.removeClass('switched');
-        mml2.removeClass('switched');
-        mml3.removeClass('switched');
-        $('.header-menu li a').click(function(){
-            mobMenu.removeClass('fixed');
-            headNav.removeClass('nav-active');
-            mml1.removeClass('switched');
-            mml2.removeClass('switched');
-            mml3.removeClass('switched');
-        })
-    }
-});
+// mobMenu.on('click', function () {
+//     if (!headNav.hasClass('nav-active')) {
+//         mobMenu.addClass('fixed');
+//         headNav.addClass('nav-active');
+//         mml1.addClass('switched');
+//         mml2.addClass('switched');
+//         mml3.addClass('switched');
+//     } else {
+//         mobMenu.removeClass('fixed');
+//         headNav.removeClass('nav-active');
+//         mml1.removeClass('switched');
+//         mml2.removeClass('switched');
+//         mml3.removeClass('switched');
+//         $('.header-menu li a').click(function(){
+//             mobMenu.removeClass('fixed');
+//             headNav.removeClass('nav-active');
+//             mml1.removeClass('switched');
+//             mml2.removeClass('switched');
+//             mml3.removeClass('switched');
+//         })
+//     }
+// });
 // Видео
 $(".video-wrapper-video").click(function () {
        
@@ -72,7 +72,7 @@ $(".video-wrapper-video").click(function () {
             //     slidesToShow: 2
             // }},
             {
-            breakpoint: 651,
+            breakpoint: 1200,
             settings: {
                 slidesToShow: 1
             }
